59d02d5a99804aa52dd8153ab369f8461ceca0a0
qbqle <132466163+qbqle@users.noreply.github.com>
2023-05-03 15:59:22 +0200

    Update streamlit_app.py
    changed header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My office cubicle')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
