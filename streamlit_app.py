34b8be55b1e15b814831e742fad79450ada77ef4
qbqle <132466163+qbqle@users.noreply.github.com>
2023-05-09 17:37:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,8 @@ streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_selected)
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
